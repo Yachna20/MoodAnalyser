584ea3dc83d9f44160afab953615b7e192f1c088
Yachna20 <thakreyachna20@gmail.com>
2023-01-08 13:51:15 +0530

    [Yachna]-Ability to analyse and respond Happy or Sad mood

---
+++ MoodAnalyser/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyzer");
+            AnalyseMood an = new AnalyseMood();
+            an.Mood("sad");
+        }
+
+    }
+}
